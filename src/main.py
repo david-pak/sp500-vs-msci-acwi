8fd60d306566a24da2a3b4fff9cdd3d95030b04f
david-pak <davidpak.connect@gmail.com>
2025-10-05 19:26:46 +0100

    Added best/worst months

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import math
 
 START_DATE = "02/1992"
 END_DATE = "07/2025"
@@ -23,18 +24,34 @@ acwi_prices = acwi["MSCI ACWI"].loc[START_DATE : END_DATE]
 sp_rt = sp_prices.pct_change().fillna(0.0)
 acwi_rt = acwi_prices.pct_change().fillna(0.0)
 
+#Calculate annual standard deviation
+sp_std = sp_rt.std(ddof = 1) * math.sqrt(12)
+acwi_std = acwi_rt.std(ddof = 1) * math.sqrt(12)
+
 #Caculate cumulative wealth
 sp_cwealth = (1 + sp_rt).cumprod()
 acwi_cwealth = (1 + acwi_rt).cumprod()
+
+#Combine cumulative wealth
+both_cwealth = pd.concat([sp_cwealth, acwi_cwealth], axis = 1, join = "inner")
+both_cwealth.index = pd.to_datetime(both_cwealth.index, format="%m/%Y")
 
 #Calculate Compound Annual Growth Rate
 sp_cagr = (sp_cwealth.iloc[-1]) ** (1 / TOTAL_YEARS) - 1
 acwi_cagr = (acwi_cwealth.iloc[-1]) ** (1 / TOTAL_YEARS) - 1
 
-#Plot cumulative wealth of indicies
-both_cwealth = pd.concat([sp_cwealth, acwi_cwealth], axis = 1, join = "inner")
-both_cwealth.index = pd.to_datetime(both_cwealth.index, format="%m/%Y")
+#Calculate best/worst months
+sp_bdate = sp_rt.idxmax()
+sp_bvalue = sp_rt.loc[sp_bdate]
+sp_wdate = sp_rt.idxmin()
+sp_wvalue = sp_rt.loc[sp_wdate]
 
+acwi_bdate = acwi_rt.idxmax()
+acwi_bvalue = acwi_rt.loc[acwi_bdate]
+acwi_wdate = acwi_rt.idxmin()
+acwi_wvalue = acwi_rt.loc[acwi_wdate]
+
+#Plot cumulative wealth of indicies
 plt.figure(figsize = (10, 5))
 plt.title("Growth of $1 from " + START_DATE + "-" + END_DATE)
 plt.plot(both_cwealth.index, both_cwealth.iloc[:, 0].values, label = both_cwealth.columns[0])
